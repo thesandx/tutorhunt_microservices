cd49c60f72ec33d20a79cd2c010bfb62ea7834c0
abhi93wari <68701254+abhi93wari@users.noreply.github.com>
2021-02-17 03:52:24 +0530

    Create tutorresponse.java

---
+++ feign/src/main/java/com/iiitb/feign/payloads/tutorresponse.java
@@ -0,0 +1,19 @@
+package com.iiitb.feign.payloads;
+
+import java.io.Serializable;
+
+public class tutorresponse {
+    private String response;
+
+    public tutorresponse(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response  ;
+    }
+}
