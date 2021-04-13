750cbf60226e298be33a3fd131773c2383163cbf
abhi93wari <abhinav6138237@gmail.com>
2021-04-13 11:10:13 +0530

    Booking Module

---
+++ feign/src/main/java/com/iiitb/feign/payloads/BookingResponse.java
@@ -0,0 +1,22 @@
+package com.iiitb.feign.payloads;
+
+import java.util.List;
+
+public class BookingResponse {
+    private List<String> response;
+
+    public List<String> getResponse() {
+        return response;
+    }
+
+    public void setResponse(List<String> response) {
+        this.response = response;
+    }
+
+    public BookingResponse(List<String> response) {
+        this.response = response;
+    }
+}
+
+
+
---
+++ feign/src/main/java/com/iiitb/feign/payloads/Bookingrequest.java
@@ -0,0 +1,69 @@
+package com.iiitb.feign.payloads;
+
+import javax.validation.constraints.NotBlank;
+
+public class Bookingrequest {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private Integer courseid;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCourseid(Integer courseid) {
+        this.courseid = courseid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getCourseid() {
+        return courseid;
+    }
+
+    public Bookingrequest(@NotBlank String name, @NotBlank Integer courseid) {
+        this.name = name;
+        this.courseid = courseid;
+    }
+}
+
+//
+//package com.iiitb.tutorhunt.payloads;
+//
+//        import javax.validation.constraints.NotBlank;
+//
+//public class courserequest {
+//
+//    @NotBlank
+//    private String coursename;
+//
+//    @NotBlank
+//    private String objective;
+//
+//
+//    public String getCoursename() {
+//        return coursename;
+//    }
+//
+//    public void setCoursename(String coursename) {
+//        this.coursename = coursename;
+//    }
+//
+//    public String getObjective() {
+//        return objective;
+//    }
+//
+//    public void setObjective(String objective) {
+//        this.objective = objective;
+//    }
+//
+//    public courserequest(@NotBlank String coursename, @NotBlank String objective) {
+//        this.coursename = coursename;
+//        this.objective = objective;
+//    }
+//}
+
